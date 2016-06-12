b80112b0b777529cb6e448bac1ffc725e32fafc7
stalin18 <kunalswami@live.in>
2016-06-13 04:21:27 +0530

    Initial Commit

---
+++ SPOJ-Programs/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SPOJ-Programs
+//
+//  Created by Kunal Swami on 13/06/16.
+//  Copyright © 2016 SHREE-GANESH. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
